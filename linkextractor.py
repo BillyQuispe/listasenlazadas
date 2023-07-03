461c3974d4afef85b3e4f4135960e27947a09d46
BillyQuispe <carlos.quispe.v@tecsup.edu.pe>
2023-07-02 23:39:15 -0500

    sda

--- linkextractor.py
+++ linkextractor.py
@@ -3,35 +3,39 @@
 import sys
 import requests
 import json
-from urllib.parse import urljoin
-class nodo:
-    def __init__(self, text, href):
+
+class Nodo:
+    def _init_(self, text, href):
         self.text = text
         self.href = href
         self.next = None     
-flag = False
+
 def extract_links(lmt=8):
-    apikey = "YWIxZjAwZDktOTEwMS00NmNiLWFmN2ItNDdlOWRiM2IzZDEz";    global flag;
+    apikey = "YWIxZjAwZDktOTEwMS00NmNiLWFmN2ItNDdlOWRiM2IzZDEz"
+    flag = False
     search_term = "excited"
-    r = requests.get("https://reqres.in/api/users/%s&limit=%s" % (search_term, lmt))
+    r = requests.get("https://reqres.in/api/users?%s&limit=%s" % (search_term, lmt))
     if r.status_code == 200:
         top_8gifs = json.loads(r.content)
-        for x in range (240):
-            for x in range(len(top_8gifs['results'])):
-                if flag == False:
-                    head = nodo(top_8gifs['results'][x]['id'], top_8gifs['results'][x]['media'][0]['webm']['preview'])
-                    flag = True;    
-                    temp = head
-                else:
-                    temp.next = nodo(top_8gifs['results'][x]['id'], top_8gifs['results'][x]['media'][0]['webm']['preview'])
-                    temp = temp.next
+        head = None
+        temp = None
+        for x in range(len(top_8gifs['data'])):
+            if flag == False:
+                head = Nodo(top_8gifs['data'][x]['id'], top_8gifs['data'][x]['avatar'])
+                flag = True
+                temp = head
+            else:
+                temp.next = Nodo(top_8gifs['data'][x]['id'], top_8gifs['data'][x]['avatar'])
+                temp = temp.next
     return head
+
 links = extract_links(12)
+
 def extrae(id):
     temp = links
     t = 0
-    while id>t:
+    while id > t:
         temp = temp.next
-        links1 = [{"text":temp.text, "href":temp.href}]
-        t=t+1
-    return links1
+        links1 = [{"text": temp.text, "href": temp.href}]
+        t += 1
+    return links1
